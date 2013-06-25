2479e7d181a13529b8bd18213ebb192a54a5ffab
Michele Piccirillo <michele.piccirillo@gmail.com>
2013-06-25 19:04:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+mirror
+======
+*A java.beans emulation library for Android*
+
+Derived from the [MadRobot Android library](https://github.com/lordofthelake/mad-robot), originally by Elton Kent.
---
+++ src/main/java/it/michelepiccirillo/mirror/reflect/ObjectAccessException.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2012 MadRobot.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the GNU Lesser Public License v2.1
+ *  which accompanies this distribution, and is available at
+ *  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *  
+ *  Contributors:
+ *  Elton Kent - initial API and implementation
+ ******************************************************************************/
+
+package it.michelepiccirillo.mirror.reflect;
+
+public class ObjectAccessException extends RuntimeException {
+	public ObjectAccessException(String message) {
+		super(message);
+	}
+
+	public ObjectAccessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
